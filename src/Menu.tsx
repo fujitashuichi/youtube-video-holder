d1f8422d6875e30477e5afca2109299322424aca
fujitashuichi <fujitashuu14@gmail.com>
2025-10-21 19:59:42 +0900

    Fix: ビデオサイズ調整の不具合を解消

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -24,6 +24,7 @@ function Menu() {
     const { videoWidth, setVideoWidth } = useContext(VideoProperty);
 
     const [displayValue, setDisplayValue] = useState("560");
+    const [maxWidth, setMAxWidth] = useState(windowWidth - 60);
 
     const handleChangeWidth = (e: React.ChangeEvent<HTMLInputElement>): void => {
         const newWidth: string = e.target.value + "px";
@@ -33,28 +34,31 @@ function Menu() {
 
     // ビデオサイズを既定の大きさにする関数
     const resetVideoWidth = useCallback(() => {
+        setMAxWidth(windowWidth - 60);
         if (windowWidth >= 610) {
             setVideoWidth("560px");
         } else {
-            const newWidth = String(windowWidth -60) + "px";
+            const newWidth = String(windowWidth - 60) + "px";
             setVideoWidth(newWidth);
         }
-        setDisplayValue(videoWidth);
-    }, [windowWidth, videoWidth, setVideoWidth, setDisplayValue]);
+    }, []);
 
     // ロード時の処理
     useEffect(() => {
-        const handleLoading = () => {
+        setWindowWidth(window.innerWidth);
+        const initializeValues = () => {
+            setMAxWidth(window.innerWidth - 60);
             resetVideoWidth();
         };
 
-        window.addEventListener("load", handleLoading);
-    }, [resetVideoWidth]);
+        window.addEventListener("load", initializeValues);
+    }, []);
 
     // リサイズイベントが発生したときの処理
     useEffect(() => {
         const handleResize = () => {
             setWindowWidth(window.innerWidth);
+            setMAxWidth(windowWidth - 60);
             if (windowWidth < 610) {
                 const newWidth = String(windowWidth - 60) + "px";
                 setVideoWidth(newWidth);
@@ -69,6 +73,11 @@ function Menu() {
         };
     }, [setVideoWidth, windowWidth]);
 
+    // videoWidth更新時の処理
+    useEffect(() => {
+        setDisplayValue(videoWidth);
+    }, [videoWidth]);
+
 
     return (
         <div id='menu'>
@@ -78,7 +87,7 @@ function Menu() {
                     <li>
                         <h3 className='li-title'>Video Size</h3>
                         <div id='range-display'>size: {displayValue}</div>
-                        <input type="range" id="iframeWidth" min="0" max={windowWidth - 50} step="1" value={videoWidth.replace("px", "")} onChange={handleChangeWidth} />
+                        <input type="range" id="iframeWidth" min="0" max={maxWidth} step="1" value={videoWidth.replace("px", "")} onChange={handleChangeWidth} />
                         <div className='reset-btn' onClick={resetVideoWidth}>reset</div>
                     </li>
                     <li>
